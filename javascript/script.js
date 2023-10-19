9fcfe39949a8e86c3674f0019120b90b45cb746e
iaio998 <depalatisiacopo@gmail.com>
2023-10-19 16:29:28 +0200

    Ticket open

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,27 @@
 myCampoMinato();
 
 function myCampoMinato() {
+  const NUM_BOMBS = 16;
+  const bombs = [];
   const btn = document.querySelector("button");
   btn.addEventListener("click", myFunctionPlayground);
 
-  //GIOCA CAMPO MINATO SU CLICK
+  //GIOCA CAMPO MINATO
   function myFunctionPlayground() {
     const totSquare = parseInt(document.querySelector("select").value);
     const bigBox = document.getElementById("playground");
     bigBox.innerHTML = "";
 
-    myBombGenerator(totSquare);
+    let bomb = myBombGenerator(bombs, totSquare);
 
     for (let i = 0; i < totSquare; i++) {
-      let boxes = myDrawSquare(i, totSquare);
+      let boxes = myDrawSquare(i, totSquare, bomb);
       bigBox.append(boxes);
     }
   }
 
   //DISEGNA QUADRATO
-  function myDrawSquare(index, totSquare) {
+  function myDrawSquare(index, totSquare, bomb) {
     const squareWidth = Math.sqrt(totSquare);
     const square = document.createElement("div");
     square.classList.add("square");
@@ -27,24 +29,32 @@ function myCampoMinato() {
     square.style.height = square.style.width;
     square.innerHTML = index + 1;
     square.addEventListener("click", function () {
-      square.classList.add("active");
-      console.log(this.innerHTML);
+      if (bomb.includes(index)) {
+        square.classList.add("bomb");
+      } else {
+        square.classList.add("active");
+        console.log(this.innerHTML);
+      }
     });
     return square;
   }
 
   //CREAZIONE BOMBE
-  function myBombGenerator(totSquare) {
-    const NUM_BOMBS = 16;
-    const bombs = [];
-    while (bombs.length < NUM_BOMBS) {
+  function myBombGenerator(bombArray, totSquare) {
+    while (bombArray.length < NUM_BOMBS) {
       let bomb = getRndInteger(1, totSquare);
-      if (!bombs.includes(bomb)) {
-        bombs.push(bomb);
+      if (!bombArray.includes(bomb)) {
+        bombArray.push(bomb);
       }
     }
-    console.log(bombs);
+    console.log(bombArray);
+    return bombArray;
   }
 
   //ESPLOSIONE BOMBA
+  // function myBombActivation() {
+  //   if (square === bombs[i]) {
+  //     square.classList.add("bomb");
+  //   }
+  // }
 }
